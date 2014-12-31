5b6e9a755b7ce6426f7fbac9d221729100f75317
Daniel Corn <d@cundd.net>
2014-12-31 19:33:54 +0100

    Added Expand interfaces to the DI configuration

--- Classes/Configuration/dependencyInjectionConfiguration.php
+++ Classes/Configuration/dependencyInjectionConfiguration.php
@@ -7,13 +7,15 @@
  */
 $persistentObjectStoreClassBase = 'Cundd\\PersistentObjectStore\\';
 return array(
-    $persistentObjectStoreClassBase . 'Formatter\\FormatterInterface'     => DI\object($persistentObjectStoreClassBase . 'Formatter\\Formatter'),
-    $persistentObjectStoreClassBase . 'Server\\Handler\\HandlerInterface' => DI\object($persistentObjectStoreClassBase . 'Server\\Handler\\Handler'),
+    $persistentObjectStoreClassBase . 'Formatter\\FormatterInterface'               => DI\object($persistentObjectStoreClassBase . 'Formatter\\Formatter'),
+    $persistentObjectStoreClassBase . 'Server\\Handler\\HandlerInterface'           => DI\object($persistentObjectStoreClassBase . 'Server\\Handler\\Handler'),
     //$persistentObjectStoreClassBase . 'Server\\BodyParser\\BodyParserInterface' => DI\object($persistentObjectStoreClassBase . 'Server\\BodyParser\\JsonBodyParser'),
-    $persistentObjectStoreClassBase . 'DataAccess\\CoordinatorInterface'  => DI\object($persistentObjectStoreClassBase . 'DataAccess\\Coordinator'),
-    $persistentObjectStoreClassBase . 'DataAccess\\ObjectFinderInterface' => DI\object($persistentObjectStoreClassBase . 'DataAccess\\ObjectFinder'),
-    $persistentObjectStoreClassBase . 'Serializer\\SerializerInterface'   => DI\object($persistentObjectStoreClassBase . 'Serializer\\JsonSerializer'),
-    $persistentObjectStoreClassBase . 'Serializer\\SerializerInterface'   => DI\object($persistentObjectStoreClassBase . 'Serializer\\JsonSerializer'),
-    $persistentObjectStoreClassBase . 'Filter\\FilterBuilderInterface'    => DI\object($persistentObjectStoreClassBase . 'Filter\\FilterBuilder'),
-    'Evenement\\EventEmitterInterface'                                    => DI\object('Evenement\\EventEmitter'),
+    $persistentObjectStoreClassBase . 'DataAccess\\CoordinatorInterface'            => DI\object($persistentObjectStoreClassBase . 'DataAccess\\Coordinator'),
+    $persistentObjectStoreClassBase . 'DataAccess\\ObjectFinderInterface'           => DI\object($persistentObjectStoreClassBase . 'DataAccess\\ObjectFinder'),
+    $persistentObjectStoreClassBase . 'Serializer\\SerializerInterface'             => DI\object($persistentObjectStoreClassBase . 'Serializer\\JsonSerializer'),
+    $persistentObjectStoreClassBase . 'Serializer\\SerializerInterface'             => DI\object($persistentObjectStoreClassBase . 'Serializer\\JsonSerializer'),
+    $persistentObjectStoreClassBase . 'Filter\\FilterBuilderInterface'              => DI\object($persistentObjectStoreClassBase . 'Filter\\FilterBuilder'),
+    $persistentObjectStoreClassBase . 'Expand\\ExpandConfigurationBuilderInterface' => DI\object($persistentObjectStoreClassBase . 'Expand\\ExpandConfigurationBuilder'),
+    $persistentObjectStoreClassBase . 'Expand\\ExpandResolverInterface'             => DI\object($persistentObjectStoreClassBase . 'Expand\\ExpandResolver'),
+    'Evenement\\EventEmitterInterface'                                              => DI\object('Evenement\\EventEmitter'),
 );
