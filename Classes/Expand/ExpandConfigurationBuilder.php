2b4434e90abe765929b7dbe56554cc7c81f27c7f
Daniel Corn <d@cundd.net>
2014-12-31 19:33:43 +0100

    Added the Expand Configuration Builder

---
+++ Classes/Expand/ExpandConfigurationBuilder.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 29.12.14
+ * Time: 15:48
+ */
+
+namespace Cundd\PersistentObjectStore\Expand;
+
+use Cundd\PersistentObjectStore\Constants;
+use Cundd\PersistentObjectStore\Expand\Exception\InvalidExpandBuilderInputException;
+
+/**
+ * Expand configuration builder
+ *
+ * @package Cundd\PersistentObjectStore\Expand
+ */
+class ExpandConfigurationBuilder implements ExpandConfigurationBuilderInterface
+{
+    /**
+     * Build the Expand configurations from the given definition
+     *
+     * @param string $expandDefinition Definition as string (E.g. "property1-database1-foreign1/property2-database2-foreign2"
+     * @return ExpandConfigurationInterface[]
+     */
+    public function buildExpandConfigurations($expandDefinition)
+    {
+        $expandConfigurationCollection = array();
+        $expandDefinitionParts         = explode(Constants::REQUEST_EXPAND_DELIMITER, $expandDefinition);
+        foreach ($expandDefinitionParts as $currentDefinition) {
+            $currentDefinitionParts = explode(Constants::REQUEST_EXPAND_SPLIT_CHAR, $currentDefinition, 3);
+            if (count($currentDefinitionParts) < 3) {
+                throw new InvalidExpandBuilderInputException(
+                    sprintf(
+                        'Could not split definition \'%s\' into property key, database and foreign key',
+                        $currentDefinition
+                    ),
+                    1420047248
+                );
+            }
+            $expandConfigurationCollection[] = new ExpandConfiguration(
+                $currentDefinitionParts[0],
+                $currentDefinitionParts[1],
+                $currentDefinitionParts[2]
+            );
+        }
+        return $expandConfigurationCollection;
+    }
+}
